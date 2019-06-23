fc6d5431dcc9aff9c9e7e008fbde8acc431981f7
Donovan <donovanlowkeen@Donovans-MacBook-Pro.local>
2019-06-23 00:32:48 -0700

    display 5 quotes per page

--- src/components/QuoteBook/QuoteBook.tsx
+++ src/components/QuoteBook/QuoteBook.tsx
@@ -45,7 +45,7 @@ class QuoteBook extends React.Component<IQuoteBookProps, IQuoteBookState> {
     targetAuthor: '',
     targetQuote: '',
     open: false,
-    quotesPerPage: 3,
+    quotesPerPage: 5,
     currentQuotes: [],
     currentPage: 1,
   };
